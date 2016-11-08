43519a6890f8422aa8589f135ff51e38a1365455
Alex <yougooo10@gmail.com>
2016-11-09 01:41:54 +0200

    started Caesar class

---
+++ programming/Caesar/caesar.py
@@ -0,0 +1,34 @@
+import string
+
+class Caesar(object):
+    LETTER_LOWER = string.ascii_lowercase
+    LETTER_UPPER = string.ascii_uppercase
+    def __init__(self, shift=0):
+        self.shift = shift
+        self.dict_letter = {}
+
+    def build_coder(self):
+        """
+            Returns a dict that can apply a Caesar cipher to a letter.
+            The cipher is defined by the shift value. Ignores non-letter characters
+            like punctuation, numbers, and spaces.
+
+            shift: 0 <= int < 26
+            returns: dict
+        """
+        def build_pos(pos):
+            """help function for map definition,
+            pos : position letter
+            return shift position"""
+            if pos + self.shift >= 26:
+                return abs(26 - (pos + self.shift))
+            return pos + self.shift
+
+        if all([0 <= self.shift, self.shift <= 26]):
+            self.temp = map(lambda x: (Caesar.LETTER_LOWER[x], Caesar.LETTER_LOWER[build_pos(x)]), range(len(Caesar.LETTER_LOWER)))
+            for elem in self.temp:
+              self.dict_letter[elem[0]] = elem[1]
+              self.dict_letter[elem[0].upper()] = elem[1].upper()
+        else:
+            return ValueError
+        return self.dict_letter
---
+++ programming/Caesar/make_fire.py
@@ -0,0 +1,8 @@
+import caesar
+
+data = caesar.Caesar(3)
+print data.build_coder()
+
+data1 = caesar.Caesar(9)
+print data1.build_coder()
+
