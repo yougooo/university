63d7bf35f5e4efbe2589eaffa752b3a1ea831055
Alex <yougooo10@gmail.com>
2016-11-22 01:25:39 +0200

    added week2

---
+++ week/L9week2.py
@@ -0,0 +1,24 @@
+import random
+
+def game(size=100):
+  arr = range(size+1)
+  random_num = size/2
+  print 'your number is {0}?'.format(random_num)
+  while 1:
+    asw = raw_input('enter low, higth or correct: ')
+    if asw == 'low':
+      arr = arr[:arr.index(random_num)]
+      print arr
+    elif asw == 'higth':
+      arr = arr[arr.index(random_num):]
+      print arr
+    elif asw == 'correct':
+      print 'game end, your number was {0}'.format(random_num)
+      break
+    else:
+      print 'input error'
+      continue 
+    random_num = random.choice(arr)
+    print 'your number is {0}?'.format(random_num)
+
+game()
