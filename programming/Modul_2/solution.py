6f8f9a5d291de3c8a97931ee5f957cfd9224d9cf
Alex <yougooo10@gmail.com>
2016-12-07 11:38:58 +0200

    added Task3

--- programming/Modul_2/solution.py
+++ programming/Modul_2/solution.py
@@ -5,15 +5,14 @@ import seaborn as sns; sns.set()
 
 
 class MonteCarlo(object):
-    '''define model model(model_line) first, 
-       after that you can work whit object,
-       but now its just test class'''
+
     def __init__(self, point):
         self.x = np.array(point, dtype=np.float16)[:, 0]
         self.y = np.array(point, dtype=np.float16)[:, 1]
 
+########################### line block ###########################
+
     def model_line(self, n):
-        '''n: how many generations and iterations will be'''
         self.k = np.array(np.random.uniform(m.radians(75), m.radians(150), n), dtype=np.float16)
         self.b = np.array(np.random.uniform(0, 5, n), dtype=np.float16)
 
@@ -22,7 +21,6 @@ class MonteCarlo(object):
 
 
     def min_error(self):
-        '''find min error for source data(point)'''
         return min(map(lambda val: self.line_err(val[0], val[1]), zip(self.k, self.b)))
 
     def run_line(self):
@@ -34,19 +32,50 @@ class MonteCarlo(object):
         if len(arr) == 0:
             return 'Bad range'
         arr = np.array(arr)
-        return arr[:, 0].sum()/len(arr), arr[:, 1].sum()/len(arr)
+        return arr[:, 0].sum() / len(arr), arr[:, 1].sum() / len(arr)
 
-    def value(self):
+    def value_line(self):
         return self.x * self.coeff[0] + self.coeff[1]
 
+########################### sin block ###########################
+# fun like: 4 + 5 * np.sin(x * 4 + 5)
+
+    def sin_model(self, n):
+        self.a = np.random.uniform(3, 5, n)
+        self.p = np.random.uniform(6, 10, n)
+        self.c = np.random.uniform(3, 6, n)
+        self.d = np.random.uniform(1, 3, n)
+
+    def sin_err(self, a, p, c, d):
+        return sum((self.y-(a + p * np.sin(self.x * c + d))) ** 2)
+
+    def sin_min_error(self):
+        return min(map(lambda val: self.sin_err(val[0], val[1], val[2], val[3]), zip(self.a, self.p, self.c, self.d)))
+
+    def run_sin(self):
+        mi = self.sin_min_error()
+        print mi
+        self.coeff = filter(lambda val: self.sin_err(val[0], val[1], val[2], val[3]) <= mi, zip(self.a, self.p, self.c, self.d))
+
+        return self.coeff
+
+    def value_sin(self):
+        self.coeff = self.coeff[0]
+        return self.coeff[0] + self.coeff[1] * np.sin(self.x * self.coeff[2] + self.coeff[3])
+
+########################### plot block ###########################
 
     def plot_line(self):
-        fig = plt.figure(figsize=(20,10))
+        fig = plt.figure()
         ax = fig.add_subplot(111)
-        # ax.axis([5, 5, 30, 30])
-        #ax.scatter(self.x, self.y)
-        ax.plot(self.x, self.value(), color='green',label='RegresionLine')
-        ax.scatter(self.x, self.y, label='SourcePoint')
-        ax.legend('best')
+        ax.plot(self.x, self.value_line(), color='green')
+        ax.scatter(self.x, self.y)
         plt.show()
 
+    def plot_sin(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(self.x, self.value_sin())
+        ax.scatter(self.x, self.y)
+        plt.show()
+        
